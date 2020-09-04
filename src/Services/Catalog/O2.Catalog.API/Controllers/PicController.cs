d3b2ea3723748e0a971ea0c5e8618728babb5a5a
Denis Prokhorchik <denisprokhorchik@hotmail.com>
2020-09-04 10:47:12 +0300

    added PicController

---
+++ src/Services/Catalog/O2.Catalog.API/Controllers/PicController.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace O2.Catalog.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Pic")]
+    public class PicController: Controller
+    {
+        private readonly IHostingEnvironment _env;
+        public PicController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webRoot = _env.WebRootPath;
+            var path = Path.Combine(webRoot+"/Pics/","product-"+id+".png");
+            var buffer = System.IO.File.ReadAllBytes(path);
+            return File(buffer, "image/png");
+        }
+    }
+}
