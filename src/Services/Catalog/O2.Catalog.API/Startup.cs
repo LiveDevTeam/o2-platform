53c22070f29c995ea248a0dfbaf6008df2831f10
Denis Prokhorchik <denisprokhorchik@hotmail.com>
2020-09-04 15:11:49 +0300

    last changes

--- src/Services/Catalog/O2.Catalog.API/Startup.cs
+++ src/Services/Catalog/O2.Catalog.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using O2.Catalog.API.Data;
 
 namespace O2.Catalog.API
@@ -28,7 +29,8 @@ namespace O2.Catalog.API
             var database = Configuration["DatabaseName"];
             var user = Configuration["DatabaseUser"];
             var password = Configuration["DatabaseUserPassword"];
-            var connectionString = string.Format("Server={0}; Database={1}; User Id={2}; Password={3};", server, database, user, password);
+            var connectionString = string.Format("Server={0};Database={1};User Id={2};Password={3};", server, database, user, password);
+
 
             //services.AddDbContext<CatalogContext>(options => options.UseSqlServer(Configuration["ConnectionString"]));
             services.AddDbContext<CatalogContext>(options => options.UseSqlServer(connectionString));
